e1cae780415bff53303d1eb1223a28b9d96f0733
Albert Leal <albert@albertleal.com>
2013-11-18 00:43:38 +0100

    modal animation with no styles

---
+++ app/js/core/error.js
@@ -0,0 +1,17 @@
+define(["navigation"], function(navigation) {
+	__ERROR = function(){
+
+	};
+	__ERROR.prototype = {
+		errorWithMessaje:function(){
+			
+		},
+		errorWithRedirection:function(){
+
+		},
+		errorWithRedirectionWithMessage:function(){
+
+		}
+	};
+	return new __ERROR();
+});
